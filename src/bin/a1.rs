1efae6691f8b0760843d9e8a5819d26dea4abcf0
SteaceP <steacy@live.ca>
2024-11-24 18:50:26 -0500

    removed the bin folder from .gitignore

---
+++ src/bin/a1.rs
@@ -0,0 +1,25 @@
+// Topic: Functions
+//
+// Program requirements:
+// * Displays your first and last name
+//
+// Notes:
+// * Use a function to display your first name
+// * Use a function to display your last name
+// * Use the println macro to display messages to the terminal
+
+// * Use a function to display your first name
+fn display_first_name() {
+    println!("Steacy")
+}
+
+// * Use a function to display your last name
+fn display_last_name() {
+    println!("Paquette")
+}
+
+// * Use the println macro to display messages to the terminal
+fn main() {
+    display_first_name();
+    display_last_name();
+}
---
+++ src/bin/functions.rs
@@ -0,0 +1,15 @@
+// * Use a function to display your first name
+fn display_first_name() {
+    println!("Steacy")
+}
+
+// * Use a function to display your last name
+fn display_last_name() {
+    println!("Paquette")
+}
+
+// * Use the println macro to display messages to the terminal
+fn main() {
+    display_first_name();
+    display_last_name();
+}
